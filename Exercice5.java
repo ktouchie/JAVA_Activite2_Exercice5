775aa410e9edf09772a2215715519004e6f178a9
Karin Touchie <ktouchie@gmail.com>
2015-12-11 11:03:35 +0100

    DUplicate Exercice4 and create ternary operation

---
+++ Exercice5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Exercice5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir un mois (1-12):");
+        int mois = sc.nextInt();
+        int jours = (mois==2) ? 28 : ((mois<8) ? ((mois%2==1) ? 31 : 30)) : ((mois%2==1) ? 30 : 31);
+        System.out.println("Ce mois contient " + jours + " jours.");
+	}
+
+}
