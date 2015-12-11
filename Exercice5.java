71f335d2d09e8441cce9a13c611faa6bc3b37430
Karin Touchie <ktouchie@gmail.com>
2015-12-11 11:07:21 +0100

    Fix error with brackets

--- Exercice5.java
+++ Exercice5.java
@@ -5,7 +5,7 @@ class Exercice5 {
 		Scanner sc = new Scanner(System.in);
         System.out.println("Veuillez saisir un mois (1-12):");
         int mois = sc.nextInt();
-        int jours = (mois==2) ? 28 : ((mois<8) ? ((mois%2==1) ? 31 : 30)) : ((mois%2==1) ? 30 : 31);
+        int jours = (mois==2) ? 28 : ((mois<8) ? ((mois%2==1) ? 31 : 30) : ((mois%2==1) ? 30 : 31));
         System.out.println("Ce mois contient " + jours + " jours.");
 	}
 
